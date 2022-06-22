86e0fab12254b4f54eed2d06c1264cfbddc3c43a
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-06-22 12:53:00 -0500

    axios request & display for collectablesCard

--- src/components/products/CollectablesCard.jsx
+++ src/components/products/CollectablesCard.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+
+
 
 function CollectablesCard() {
+
+    const [allCollectables, setAllCollectables] = useState([])
+
+    useEffect(() => {
+        axios.get(`http://localhost:4000/api/getCollectables`).then(res => setAllCollectables(res.data))
+    }, [])
+
     return (
-        <div>CollectablesCard</div>
+        <div>
+            {allCollectables.map(collectable =>
+                <div>
+                    <img src={collectable.image} alt={collectable.product_name} />
+                    <h2>{collectable.product_name}</h2>
+                    <h3>{collectable.description}</h3>
+                    <h3>${collectable.price}</h3>
+                    <button>Add to Cart</button>
+                </div>
+                )}
+
+        </div>
     )
 }
 
