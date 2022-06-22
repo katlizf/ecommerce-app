751dce248017b2b755199b4142ef63bc6d244614
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-06-22 17:48:48 -0500

    changed background image

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import backgroundImg from '../../images/wallpaper-multi-character.png'
+import background from '../../images/bleach-kisuke-urahara-wallpaper.jpg'
 
 function HomePage() {
     return (
         <div className='bg-black' style={{
-            backgroundImage: `url(${backgroundImg})`, backgroundRepeat:'no-repeat', height:875, backgroundSize:'cover', backgroundPosition:'center'}}>
-            <p className='text-orange font-extrabold text-center text-7xl py-20 bg-black bg-opacity-50'>Welcome to Weebs R Us!</p>
+            backgroundImage: `url(${background})`, backgroundRepeat:'no-repeat', height:875, backgroundSize:'cover', backgroundPosition:'center'}}>
+            <p className='text-orange font-extrabold text-center text-6xl py-16 bg-black bg-opacity-50'>Welcome to Weebs R Us!</p>
         </div>
     )
 }
