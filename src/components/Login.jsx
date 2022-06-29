be1caf62ae9582a5b75467bcfa05193ad69668da
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-06-28 20:59:53 -0500

    working on modal styling

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,12 +3,14 @@ import ReactModal from 'react-modal'
 import ReactDOM from 'react-dom'
 import axios from 'axios'
 import {Redirect} from 'react-router-dom'
+import {data} from 'autoprefixer'
 
 
 
 function Login() {
 
     const [showModal, setShowModal] = useState(false)
+    const [userInfo, setUserInfo] = useState('')
 
     const openModal = () => {
         setShowModal(true)
@@ -19,24 +21,37 @@ function Login() {
 
     const checkUserExists = user => {
         const body = {id: user.id, username: user.username, password: user.password}
-        axios.post(`http://localhost:4000/api/login`, body).then(res => res.data)
+        axios.post(`http://localhost:4000/api/login`, body).then(res => setUserInfo(res.data))
     }
 
     return (
         <div>
             <button onClick={e => openModal()}>Login</button>
             <ReactModal
-                isOpen={showModal}>
-                <p>Login Below:</p>
-                <input
-                    name='username'
-                    type='text'
-                    placeholder='Username'></input>
-                <input
-                    name='password'
-                    type='text'
-                    placeholder='Password'></input>
-                <button onClick={closeModal}>Submit</button>
+                isOpen={showModal}
+                style={{
+                    content: {
+                        top:'50px', 
+                        left:'50px',
+                        border:'2px solid #ccc'
+                    }}}>
+                <div className='flex flex-col'>
+                    <p className='center'>Login Below or Register by Entering a New Username and Password</p>
+                    <label className='center'>Username</label>
+                    <input
+                        name='username'
+                        type='text'
+                        value={data.username}
+                        className='center-input'></input>
+                    <label className='center'>Password</label>
+                    <input
+                        name='password'
+                        type='text'
+                        value={data.password}
+                        className='center-input'></input>
+                    <button onClick={closeModal}>Submit</button>
+                </div>
+
             </ReactModal>
         </div>
     )
